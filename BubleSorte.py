f76e1dd801256742e9adfdef00ec8a544594f9dd
digspencer <diegoiglan@hotmail.com>
2023-05-25 15:55:35 -0300

    Att Bubble-Sorte

--- BubleSorte.py
+++ BubleSorte.py
@@ -1,3 +1,17 @@
 # Buble Sorte
+def bubleSorte(arr):
+    n = len(arr)
 
-contador = 1
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+if __name__ == '__main__':
+    arr = [6, 5, 4, 3, 2, 1]
+
+bubleSorte(arr)
+
+print('Sorted array is:')
+for i in range(len(arr)):
+    print("%d" % arr[i], end=" ")
